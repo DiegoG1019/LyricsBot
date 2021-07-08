b9cff26ff92e22ce8d7fa76f35754376111d2bb6
Diego García <dagarciam1014@gmail.com>
2021-07-08 03:16:46 -0400

    Update Program.cs

--- LyricsBot/LyricsBot/Program.cs
+++ LyricsBot/LyricsBot/Program.cs
@@ -9,6 +9,8 @@ namespace LyricsBot
         {
             await LyricsBot.Init();
 
+#error You ain't loading the configurations file
+
             while (true)
                 await Task.Delay(200); //It really doesn't have much else to do other than to wait for input, and everything else is done on background threads
         }
