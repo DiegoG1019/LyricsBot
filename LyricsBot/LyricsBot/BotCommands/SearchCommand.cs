c447da4ca9634c4517064cfad7dcf5ba29ea437c
Diego García <dagarciam1014@gmail.com>
2021-07-14 03:06:23 -0400

    Added a list of possible sources in WebScraper, began SearchCommand work

---
+++ LyricsBot/LyricsBot/BotCommands/SearchCommand.cs
@@ -0,0 +1,56 @@
+﻿using DiegoG.TelegramBot;
+using DiegoG.TelegramBot.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot.Types;
+
+namespace LyricsBot.BotCommands
+{
+#error Es aqui la verga @Jal
+
+    //[BotCommand]
+    class SearchCommand : IBotCommand
+    {
+        public BotCommandProcessor Processor { get; set; }
+
+//You can delete these
+#error Write The things
+
+        public string HelpExplanation => "Write something here";
+
+
+        public string HelpUsage => "Write something here";
+
+        public IEnumerable<(string Option, string Explanation)>? HelpOptions => new (string, string)[]
+        {
+            ("OptionName", "Explain like I'm 5"),
+            //Add more here, if necessary
+        };
+
+        public string Trigger => "/search";
+
+        public string? Alias => null; //You can also set something here, usually a shorter version of /search, like /s? Purely optional. Beware of trigger collisions
+
+        //the "Hold" portion of the tuple is to tell the processor to hold this command for a conversation to that specific user. Usually, you'd keep your own User Dictionary to maintain a state inside this class. The Processor will tell you which user it is, but nothing else. But you probably won't need the Hold, so set it to false each time you return
+
+        public Task<(string Result, bool Hold)> Action(BotCommandArguments args)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<(string Result, bool Hold)> ActionReply(BotCommandArguments args)
+        {
+            throw new NotImplementedException(); //If you're not gonna need to hold a conversation, you can leave this one as is. If it throws this exception, you did something wrong somewhere else. 
+        }
+
+        //This is for clearing any state info you may have in here, and it's called manually. Usually for when you Hold a conversation
+
+        public void Cancel(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- LyricsBot/LyricsBot/BotCommands/WebScraper.cs
+++ LyricsBot/LyricsBot/BotCommands/WebScraper.cs
@@ -6,6 +6,8 @@ using Serilog;
 using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
 using System.Net;
 using System.Runtime.CompilerServices;
 using System.Text;
@@ -19,12 +21,53 @@ namespace LyricsBot.BotCommands
     [BotCommand]
     public class WebScraper : Default
     {
-        private const string BaseURL = "https://genius.com/amp/{0}-lyrics";
-        private static async Task<string> GetURL(string args)
-            => string.Format(BaseURL, await Task.Run(() => Regex.Replace(args, @"[\s+]?[-][\s+]? | \s?", "-").ToLower()).AwaitWithTimeout(1000));
+        private const string GeniusBaseURL = "https://genius.com/amp/{0}-lyrics";
+        private static async Task<string> GetGenius(string args)
+            => string.Format(GeniusBaseURL, await Task.Run(() => Regex.Replace(args, @"[\s+]?[-][\s+]? | \s?", "-").ToLower().Replace("\"", "")).AwaitWithTimeout(1000));
+
+        private const string MusixmatchBaseUrl = "https://www.musixmatch.com/lyrics/{0}/{1}";
+        private static async Task<string> GetMusixmatch(string args)
+        {
+            var (artist, song) = await Task.Run(() =>
+            {
+                args = args.ToLower().Replace("\"", "");
+                List<int> dashes = new(args.Length / 2);
+                int count = 0;
+                for (int i = 0; i < args.Length; i++)
+                    if (args[i] is '-')
+                    {
+                        count++;
+                        dashes.Add(i);
+                    }
+
+                if (count < 1)
+                    throw new InvalidBotCommandArgumentsException(args, "Please tell me the name of the song like ArtistName - SongName");
+
+                var song = args[dashes[count / 2]..].Trim();
+                var artist = args[..dashes[count / 2]].Trim();
+
+                return (artist.Replace(' ', '-').Replace("\"", ""), song.Replace(' ', '-').Replace("\"", ""));
+            }).AwaitWithTimeout(1000);
+            return string.Format(MusixmatchBaseUrl, artist, song);
+        }
+
         private static readonly HtmlDocument HtmlDoc = new();
 
         private readonly TemporaryCache<string, string> LyricsCache = new(TimeSpan.FromMinutes(5));
+
+        private bool FetchCache([NotNullWhen(true)] out string? lyrics, params string[] checks)
+            => FetchCache(out lyrics, checks);
+        private bool FetchCache([NotNullWhen(true)] out string? lyrics, IEnumerable<string> checks)
+        {
+            foreach(var s in checks)
+                if(LyricsCache.ContainsKey(s))
+                {
+                    lyrics = LyricsCache[s];
+                    return true;
+                }
+            lyrics = null;
+            return false;
+        }
 
         public override async Task<(string, bool)> Action(BotCommandArguments args)
         {
@@ -33,44 +76,58 @@ namespace LyricsBot.BotCommands
             {
                 Log.Information($"Attempting to find lyrics for user {args.User}");
 
-                string url;
+                List<string> urls = new();
                 try
                 {
-                    url = await GetURL(args.ArgString);
+                    {
+                        var gt = GetGenius(args.ArgString);
+                        urls.Add(await GetMusixmatch(args.ArgString));
+                        urls.Add(await gt);
+                    }
+
+                    if (FetchCache(out string? lyrics, urls))
+                    {
+                        Log.Debug($"Retrieving lyrics from cache");
+                        return (lyrics, false);
+                    }
+                    else
+                    {
+                        WebClient MyWeb = new();
+                        foreach (var url in urls)
+                        {
+#warning This is Untested @Jal
+                            try
+                            {
+                                Log.Debug($"Downloading lyrics page from {url}");
+                                string Data = MyWeb.DownloadString(url);
+                                HtmlDoc.LoadHtml(Data);
+                                MyWeb.Dispose();
+                                Log.Verbose($"Searching for \"{args.ArgString}\" ({url})");
+                                var HeaderLyrics = HtmlDoc.DocumentNode.SelectSingleNode("//div[@class='lyrics']");
+                                lyrics = HeaderLyrics.InnerText;
+                                LyricsCache.Add(url, lyrics);
+                                Log.Information($"Found Lyrics for user {args.User}");
+
+                                MyWeb.Dispose();
+                                return (lyrics, false);
+                            }
+                            catch (Exception)
+                            {
+                                Log.Verbose($"Could not find Lyrics for User {args.User} in {url}");
+                            }
+                        }
+
+                        Log.Information($"Could not find Lyrics for User {args.User}");
+                        
+                        MyWeb.Dispose();
+                        return ("Could not find the requested song/artist", false);
+                    }
                 }
                 catch (TimeoutException e)
                 {
                     Log.Error(e, "A TimeoutException ocurred while parsing the request");
                     return ("There was a problem parsing the request, please try again. Regex timed out.", false);
                 }
-
-                string lyrics;
-                if (LyricsCache.ContainsKey(url))
-                {
-                    Log.Debug($"Retrieving {url} lyrics from cache");
-                    lyrics = LyricsCache[url];
-                }
-                else
-                {
-                    Log.Debug($"Downloading lyrics page from {url}");
-                    WebClient MyWeb = new();
-                    string Data = MyWeb.DownloadString(url);
-                    HtmlDoc.LoadHtml(Data);
-                    MyWeb.Dispose();
-                    Log.Verbose($"Searching for \"{args.ArgString}\" ({url})");
-                    var HeaderLyrics = HtmlDoc.DocumentNode.SelectSingleNode("//div[@class='lyrics']");
-                    lyrics = HeaderLyrics.InnerText;
-                    LyricsCache.Add(url, lyrics);
-                }
-
-                Log.Information($"Found Lyrics for user {args.User}");
-
-                return (lyrics, false);
-            }
-            catch (Exception)
-            {
-                Log.Information($"Could not find Lyrics for User {args.User}");
-                return ("Could not find the requested song/artist", false);
             }
             finally
             {
